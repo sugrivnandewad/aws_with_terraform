3893768f176d241cf0ec6772e2f524f34589cbef
Sugriv Nandewad <sugriv.nandewad@alignedautomation.com>
2026-01-14 15:47:43 +0530

    code changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@
 # Root configuration for the dev environment
 module "vpc" {
   source   = "../../modules/vpc"
-  region   = "us-west-2"
+  region   = "ap-south-1"
   vpc_cidr = "10.167.0.0/16"
   vpc_name = "dev-vpc"
 }
