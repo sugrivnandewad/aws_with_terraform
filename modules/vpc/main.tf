bbebd45df8d7a0582a1441409271b5805efea935
Sugriv Nandewad <sugriv.nandewad@alignedautomation.com>
2026-01-14 13:04:51 +0530

    aws code for private subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,3 +57,50 @@ resource "aws_route_table_association" "public_rt_assoc" {
     subnet_id = aws_subnet.public_subnet[count.index].id
     route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_subnet" "private_subnet" {
+    count = length(data.aws_availability_zones.available.names)
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + length(data.aws_availability_zones.available.names))
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = false
+    
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index + 1}"
+  }
+  
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "${var.vpc_name}-private-rt"
+    }
+}
+
+resource "aws_route" "private_route" {
+    route_table_id = aws_route_table.private_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet[0].id
+    tags = {
+        Name = "${var.vpc_name}-nat-gw"
+    }
+  
+}
+resource "aws_eip" "nat_eip" {
+    vpc = true
+    tags = {
+        Name = "${var.vpc_name}-nat-eip"
+    }
+  
+}
+resource "aws_route_table_association" "private_rt_assoc" {
+    count = length(aws_subnet.private_subnet)
+    subnet_id = aws_subnet.private_subnet[count.index].id
+    route_table_id = aws_route_table.private_rt.id
+}
