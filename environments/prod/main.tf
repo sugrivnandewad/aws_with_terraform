3c8cac93e7c8368e42c72badeaebb2d133980340
Sugriv Nandewad <sugriv.nandewad@alignedautomation.com>
2026-01-14 15:55:53 +0530

    added prod vpc

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,2 +1,8 @@
 # Prod environment - main.tf
 # Root configuration for the prod environment
+module "vpc" {
+  source   = "../../modules/vpc"
+  region   = "ap-south-1"
+  vpc_cidr = "10.168.0.0/16"
+  vpc_name = "prod-vpc"
+}
